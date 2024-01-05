713bf7c89419e19ea98b011f0b82216e7525d897
Tanjil Ahamed <tanjil113355@gmail.com>
2024-01-05 15:46:27 +0600

    recommendation section added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,14 +1,17 @@
+
+import footerImage from '../../../public/Image2.png'
+
 const Footer = () => {
     return (
-        <footer className="p-10 bg-orange-400">
-            <div>
-                <div>
-                    <input type="text" />
-                    <button className="">Subscribe</button>
+        <footer className="p-10 bg-orange-400 flex justify-center gap-20 items-center">
+            <div className='w-1/2'>
+                <div className='relative'>
+                    <input className='p-3 rounded-full w-full' type="text" />
+                    <button className="p-2 bg-orange-500 rounded-2xl absolute top-1 right-5">Subscribe</button>
                 </div>
             </div>
-            <div>
-                <img src="" alt="" />
+            <div className='w-1/2'>
+                <img className='w-1/2 mx-auto' src={footerImage} alt="" />
             </div>
         </footer>
     );
